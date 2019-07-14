b2f5303ae0a8c9b6c244d20cf1d76a82f02433bd
jeff MUsk <jeffprogramacion@gmail.com>
2019-07-14 11:43:33 -0500

    agregadas todas las carpetas del proyecto

---
+++ src/comp/App/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter , Route , Switch, Redirect } from 'react-router-dom';
+import Home from '../Home/Home';
+import {  Container } from 'reactstrap';
+
+
+
+function App() {
+  return (
+    <BrowserRouter >  
+           
+
+      <Container fluid={false}  >
+        <Switch>
+          
+        <Route exact path="/" component={Home} /> 
+          
+          
+          />      
+        </Switch>
+      </Container>
+    </BrowserRouter>  
+  );
+}
+
+export default App;
---
+++ src/comp/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+export default class Nav extends Component {
+
+    render() {
+        return (
+            <div className="Nav">
+                <h1>Nav</h1>
+            </div>
+        );
+    }
+}
