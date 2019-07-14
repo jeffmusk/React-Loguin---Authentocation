1c402550b48911cb6a2f4ed07c83c86e49e39a81
jeff MUsk <jeffprogramacion@gmail.com>
2019-07-14 11:22:47 -0500

    Proyecto en cero solo HOME

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter , Route , Switch, Redirect } from 'react-router-dom';
+import Home from './comp/Vistas/Home/Home';
+import {  Container } from 'reactstrap';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter >  
+           
+
+      <Container fluid={false}  >
+        <Switch>
+          
+        <Route exact path="/" component={Home} /> 
+          
+          
+          />      
+        </Switch>
+      </Container>
+    </BrowserRouter>  
   );
 }
 
